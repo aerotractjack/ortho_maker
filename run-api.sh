8afecbf7cfc5bb133708d1856e20ec96b6b8f47d
jack <jackw@aerotractone.com>
2023-05-10 14:59:34 -0700

    work

--- run-api.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-PORT=500"${HOSTNAME: -1}"
-FLASK_APP=api.py /home/aerotract/.local/bin/flask run --host=0.0.0.0 --port=$PORT
--- run-dashboard.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-FLASK_APP=dashboard.py /home/aerotract/.local/bin/flask run --host=0.0.0.0 --port=5000
