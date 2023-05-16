ebb9cb45cca53982eb64ee20fa2280695c8287d3
Jack Wolf <jackw@aerotractone.com>
2023-05-16 15:24:18 -0700

    making complete page

--- dashboard.py
+++ dashboard.py
@@ -13,9 +13,10 @@ complete_Q = OrthoQ("/home/aerotract/NAS/main/OrthoQ_finished")
 
 @app.after_request
 def add_header(response):
-    response.headers['Cache-Control'] = 'no-store'
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
     return response
-
 
 @app.route("/index")
 def index():
